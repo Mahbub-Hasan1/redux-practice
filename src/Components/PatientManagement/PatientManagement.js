55e9fe0a665921aff01646c844c62e13851c13ad
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2020-11-29 04:32:21 -0800

    done

--- src/Components/PatientManagement/PatientManagement.js
+++ src/Components/PatientManagement/PatientManagement.js
@@ -1,14 +1,33 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 import { patientReducer, patientState } from '../../Reducer/PatientReducer';
 
 const PatientManagement = () => {
+    const nameRef = useRef();
     const [state, dispatch] = useReducer(patientReducer, patientState);
+    const handleSubmit = event => {
+        event.preventDefault();
+        dispatch({
+            type: 'ADD_PATIENT',
+            name: nameRef.current.value,
+            id: state.patients.length + 1
+        })
+        nameRef.current.value = '';
+    }
     return (
         <div>
             <h1>patient Management{state.patients.length}</h1>
-            <from>
-                <input></input>
-            </from>
+            <form onSubmit={handleSubmit}>
+                <input ref={nameRef} ></input>
+            </form>
+            {
+                state.patients.map(pt =>
+                    <li key={pt.id}
+                    >{pt.name}-
+                        <button onClick={() => dispatch({ type: 'REMOVE_PATIENT', id: pt.id })}
+                        >Delete</button>
+                    </li>
+                )
+            }
         </div>
     );
 };
